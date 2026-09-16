2a33547548db6820c8dd4ec0e1435e141d0cb77a
mike-axiom-mir <miketobi90@gmail.com>
2026-09-16 04:25:35 +0200

    Expose organic form study API

---
+++ src/axm_nature_design/__init__.py
@@ -0,0 +1,3 @@
+from .organic_form import build_evidence, build_mesh, design_checks, digest, load_source, structural_checks
+
+__all__ = ["build_evidence", "build_mesh", "design_checks", "digest", "load_source", "structural_checks"]
